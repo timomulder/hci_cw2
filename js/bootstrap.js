90f2446abd3909b7527c984c3748e99cd6de3562
timomulder <tmamulder@gmail.com>
2016-11-09 23:53:18 +0000

    mobile friendly

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -671,11 +671,6 @@ if (typeof jQuery === 'undefined') {
 
   Collapse.prototype.toggle = function () {
     this[this.$element.hasClass('in') ? 'hide' : 'show']()
-    $('.collapse').on('shown.bs.collapse', function(){
-    $(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
-    }).on('hidden.bs.collapse', function(){
-    $(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
-    });
   }
 
   Collapse.prototype.getParent = function () {
